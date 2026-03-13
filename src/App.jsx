1247ae53710d1e765ed057b6878f9e9b6c0c63d3
Shashank Mandoli <shashankmandoli2004@gmail.com>
2026-03-14 00:37:25 +0530

    Add InputBox.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 function App() {
   
-    
-
   return (
         <>
+        <div className="bg-orange-500 ">
+          <h1 className="text-white text-4xl text-center">Currency Converter</h1>
+        </div>
         </>
   );
 }
+
+export default App
